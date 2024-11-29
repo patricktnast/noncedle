ac3a2aa6f7dd2278b0aa3b421d472ec60cf455f4
Patrick Nast <patricktnast@gmail.com>
2024-11-29 10:06:12 -0800

    simplify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import NoncedleGame from './components/NoncedleGame'
 
 const App: React.FC = () => {
-  return (
-    <div className="w-screen flex items-center justify-center bg-gray-100">
-      <div className="w-[95vw] min-h-[95vh] overflow-y-auto">
-        <NoncedleGame />
-      </div>
-    </div>
-  )
+  return <NoncedleGame />
 }
 
 export default App
